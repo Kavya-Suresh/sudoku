f914b8ebcdc527a671a0f449e3994f4665244458
Abhinav MS <mail.abhinavms@gmail.com>
2018-03-23 01:27:05 +0530

    Update sud1.c

--- sud1.c
+++ sud1.c
@@ -4,17 +4,15 @@ int puzz[9][9];
 int rows(int n,int row)
 {	int i;
 	for (i=0;i<9;i++)
-	{	if(puzz[row][i]==n) 	//check all columns of the row (1 if poss)
+		if(puzz[row][i]==n) 	//check all columns of the row (1 if poss)
 			return 0;
-	}
 	return 1;
 }
 int columns(int n,int col)
 {	int i;
 	for (i=0;i<9;i++)
-	{	if(puzz[i][col]==n) 	// check all rows in that column (1 if poss)
+	if(puzz[i][col]==n) 	// check all rows in that column (1 if poss)
 			return 0;
-	}
 	return 1;
 }
 int block(int n,int row, int col)
@@ -22,23 +20,23 @@ int block(int n,int row, int col)
 	r=(row/3)*3;
 	c=(col/3)*3;
 	for(i=r;i<r+3;i++)
-	{	for(j=c;j<c+3;j++)
-		{	if(puzz[i][j]==n)		//check block
+		for(j=c;j<c+3;j++)
+			if(puzz[i][j]==n)		//check block
 				return 0;
-		}
-	}
 	return 1;
 }
 void print()			//print
 {   int i,j;
-	for(i=0;i<9;i++)
-	{	for (j=0;j<9;j++)
-			printf("  %d",puzz[i][j]);
+    printf("The Solved Sudoku is \n");
+	for(i=0;i<9;i++){
+		for (j=0;j<9;j++)
+		printf("  %d",puzz[i][j]);
 		printf("\n");
-	}
+}
+getch();
 }
 void solve(int row,int col)
-{	int count=0,i;
+{
     if(row>8)
 		print();
 	if(puzz[row][col]!=0)
@@ -48,38 +46,25 @@ void solve(int row,int col)
 			solve(row+1,0);
 	}
 	else
-	{	for (count=0,i=0;i<9;i++)
-		{	if((rows(i,row)&&columns(i,col)&&block(i,row,col))==1)
-				count=count+1;
-		}
-		if(count==1)
-		{	puzz[row][col]=i;					// assign n=x
+	{ int i;
+		for (i=1;i<9;i++)
+		{	if((rows(i,row)&&columns(i,col)&&block(i,row,col))==1){
+			puzz[row][col]=i;					// assign n=x
 			if(col<8)
 				solve(row,col+1);
 			else
 				solve(row+1,0);
 		}
+		 }
 		puzz[row][col]=0;
 	}			
 }
 
-void main()
-{	puzz[9][9] = {{0,3,7, 0,5,2, 0,1,0},
-				  {9,0,1, 7,0,0, 0,8,6},
-				  {2,0,0, 1,0,9, 7,0,3},
-
-				  {0,0,9, 2,8,0, 5,3,0},
-				  {8,0,6, 3,0,0, 0,9,4},
-				  {5,7,0, 9,0,1, 0,6,0},
-
-				  {0,6,0, 0,2,7, 9,0,0},
-				  {7,1,0, 0,9,3, 6,0,8},
-				  {0,9,2, 0,1,0, 3,0,5}};
-	/*
-	for(i=0;i<9;i++)
-		for (j=0;j<9;j++)
-		{	printf("%d%d: "i,j);
-			scanf("%d",&Sudoku[i][j]);
-	*/
-	solve(0,0);
+int main() 
+{ int row,column;
+ printf("Enter the sudoku to be solved and enter 0 for unknown entries\n"); 
+ for(row=0;row<9;row++) 
+	 for(column=0;column<9;column++) 
+		 scanf("%d",&puzz[row][column]); 
+ solve(0,0);
 }
